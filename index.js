c09c13ef2c9ff01a3c81717466ea861406dcea64
Daniel Di Venere <danieldivenere@gmail.com>
2019-11-01 12:38:04 -0700

    added boilder plate code

---
+++ index.js
@@ -0,0 +1,16 @@
+const STORE = {
+    correctAnswers: 0,
+    incorrectAnswers: 0,
+    currentQuestion: 0
+}
+
+const QUESTIONS = [
+    {
+        question: "Who is Elon",
+        answers: [
+            "yes",
+            "no"
+        ],
+        correctAnswer: "yes"
+    }
+]
