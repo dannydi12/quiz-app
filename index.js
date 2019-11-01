c9656673a9ae9ce709bfd44cab7137be25ac2f55
Daniel Di Venere <danieldivenere@gmail.com>
2019-11-01 13:02:18 -0700

    added answers and questions

--- index.js
+++ index.js
@@ -6,11 +6,53 @@ const STORE = {
 
 const QUESTIONS = [
     {
-        question: "Who is Elon",
+        question: "Who is Elon?",
         answers: [
-            "yes",
-            "no"
+            "An entrepeneur",
+            "A billionaire",
+            "An all-around awesome guy",
+            "All of the above"
         ],
-        correctAnswer: "yes"
+        correctAnswer: "All of the above"
+    },
+    {
+        question: "Where was Elon born?",
+        answers: [
+            "The United States of America",
+            "Canada",
+            "South Africa",
+            "Australia"
+        ],
+        correctAnswer: "South Africa"
+    },
+    {
+        question: "What is Elon Musk's middle name?",
+        answers: [
+            "Reeve",
+            "Adam",
+            "Henry",
+            "Maye"
+        ],
+        correctAnswer: "Reeve"
+    },
+    {
+        question: "What was Elon Musk's first multi-million dollar company?",
+        answers: [
+            "Paypal",
+            "Zip2",
+            "Tesla",
+            "SpaceX"
+        ],
+        correctAnswer: "Zip2"
+    },
+    {
+        question: "When asked about Jeff Bezos, Elon Musk famously replied:",
+        answers: [
+            "\"I hate Amazon.\"",
+            "\"We both have space companies!\"",
+            "\"Jeff who?\"",
+            "\"I think his companies are overvalued.\""
+        ],
+        correctAnswer: "\"Jeff who?\""
     }
 ]
