ec3dcb1e62f584d80d951575c019f06703bcdabb
Daniel Di Venere <danieldivenere@gmail.com>
2019-11-02 11:22:15 -0700

    everything

---
+++ database.js
@@ -0,0 +1,57 @@
+const STORE = {
+    correctAnswers: 0,
+    currentQuestionIndex: 0
+}
+
+const QUESTIONS = [
+    {
+        question: "Who is Elon?",
+        answers: [
+            "An entrepeneur",
+            "A billionaire",
+            "An all-around awesome guy",
+            "All of the above"
+        ],
+        correctAnswer: "All of the above"
+    },
+    {
+        question: "Where was Elon born?",
+        answers: [
+            "The United States of America",
+            "Canada",
+            "South Africa",
+            "Australia"
+        ],
+        correctAnswer: "South Africa"
+    },
+    {
+        question: "What is Elon Musk's middle name?",
+        answers: [
+            "Reeve",
+            "Adam",
+            "Henry",
+            "Maye"
+        ],
+        correctAnswer: "Reeve"
+    },
+    {
+        question: "What was Elon Musk's first multi-million dollar company?",
+        answers: [
+            "Paypal",
+            "Zip2",
+            "Tesla",
+            "SpaceX"
+        ],
+        correctAnswer: "Zip2"
+    },
+    {
+        question: "When asked about Jeff Bezos, Elon Musk famously replied:",
+        answers: [
+            "&#8220I hate Amazon.&#8221",
+            "&#8220We both have space companies!&#8221",
+            "&#8220Jeff who?&#8221",
+            "&#8220I think his companies are overvalued.&#8221"
+        ],
+        correctAnswer: "“Jeff who?”"
+    }
+]
--- index.js
+++ index.js
@@ -1,161 +1,124 @@
-const STORE = {
-    correctAnswers: 0,
-    // incorrectAnswers: 0,
-    currentQuestion: 0
-}
-
-const QUESTIONS = [
-    {
-        question: "Who is Elon?",
-        answers: [
-            "An entrepeneur",
-            "A billionaire",
-            "An all-around awesome guy",
-            "All of the above"
-        ],
-        correctAnswer: "All of the above"
-    },
-    {
-        question: "Where was Elon born?",
-        answers: [
-            "The United States of America",
-            "Canada",
-            "South Africa",
-            "Australia"
-        ],
-        correctAnswer: "South Africa"
-    },
-    {
-        question: "What is Elon Musk's middle name?",
-        answers: [
-            "Reeve",
-            "Adam",
-            "Henry",
-            "Maye"
-        ],
-        correctAnswer: "Reeve"
-    },
-    {
-        question: "What was Elon Musk's first multi-million dollar company?",
-        answers: [
-            "Paypal",
-            "Zip2",
-            "Tesla",
-            "SpaceX"
-        ],
-        correctAnswer: "Zip2"
-    },
-    {
-        question: "When asked about Jeff Bezos, Elon Musk famously replied:",
-        answers: [
-            "\"I hate Amazon.\"",
-            "\"We both have space companies!\"",
-            "\"Jeff who?\"",
-            "\"I think his companies are overvalued.\""
-        ],
-        correctAnswer: "\"Jeff who?\""
-    }
-]
-
 // Components For Renders
 function generateOptionHtml(answerIndex) {
-    return `<p>
-      <input type="radio" name="answer" id="${answerIndex}" value="${QUESTIONS[STORE.currentQuestion].answers[answerIndex]}">
-      <label for="${answerIndex}">${QUESTIONS[STORE.currentQuestion].answers[answerIndex]}</label>
-    </p>`;
+    let allAnswers = QUESTIONS[STORE.currentQuestionIndex].answers; 
+    return `
+        <label for="${answerIndex}">
+            <input type="radio" name="answer" id="${answerIndex}" value="${allAnswers[answerIndex]}" required/>
+            <span>${allAnswers[answerIndex]}</span>
+        </label>`;
 }
 
 function generateAllOptionsHtml() {
-    let arrayOfOptions = QUESTIONS[STORE.currentQuestion].answers;
+    let arrayOfOptions = QUESTIONS[STORE.currentQuestionIndex].answers;
     return arrayOfOptions.map((option, i) => generateOptionHtml(i)).join('');
 }
 
-function renderWrong() {
+function wrongFeedback() {
     // Make array of insults and randomly insert them, same with right
-    return `<h2>Oof, that's wrong.</h2>
-    <p>The correct answer was: ${QUESTIONS[STORE.currentQuestion].correctAnswer}</p>
+    return `
+    <header>
+        <h2>Oof, that's wrong.</h2>
+    </header>
+    <p>The correct answer was: ${QUESTIONS[STORE.currentQuestionIndex].correctAnswer}</p>
     <button class = "next-button js-next">Next</button>`
 }
 
-function renderRight() {
+function rightFeedback() {
     STORE.correctAnswers++;
-    return `<h2>Elon approves!</h2>
-    <img src="happy-image">
-    <button class = "next-button js-next">Start</button>`
+    return `
+    <header>
+        <h2>Elon approves!</h2>
+    </header>
+    <img src="happy-image" alt>
+    <button class = "next-button js-next">Next</button>`
 }
 
 function isCorrect(givenAnswer) {
-    let correctAnswer = QUESTIONS[STORE.currentQuestion].correctAnswer;
+    let correctAnswer = QUESTIONS[STORE.currentQuestionIndex].correctAnswer;
     return givenAnswer === correctAnswer;
 }
 
 // Rendering Functions
 function renderCounter() {
-    $('.js-counter').html(`Question: ${STORE.currentQuestion + 1}/${QUESTIONS.length}`);
+    $('.js-counter').html(`Question: ${STORE.currentQuestionIndex + 1}/${QUESTIONS.length}`);
     $('.score').html(`Score: ${STORE.correctAnswers}`);
 }
 
 function renderStartPage() {
     $('.js-render').html(`
-    <h2>Start The Quiz</h2>
+    <header>
+        <h2>Start The Quiz</h2>
+    </header>
     <p>Elon Musk has been pretty popular among the public lately, and that's great. But here's the problem: he's also gaining a lot of inauthentic bandwagons. 
     This quiz was designed to weed out Elon Musk's #1 fans from the fake fans jumping in on the hype. 
     Do you think you can prove your worth? Give it a try!</p>
-    <button class = "start-button js-start">Start</button>
+    <button type="button" class = "start-button js-start">Start</button>
     `);
-    renderCounter();
+    $('.score').html('');
+    $('.js-counter').html('');
 }
 
 function renderQuestionPage() {
     $('.js-render').html(`
-    <form>
+    <form role="form" accept-charset="UTF-8">
         <fieldset>
-            <legend>${QUESTIONS[STORE.currentQuestion].question}</legend>
+            <legend>${QUESTIONS[STORE.currentQuestionIndex].question}</legend>
             ${generateAllOptionsHtml()}
         </fieldset>
-        <button class="restart-button js-restart">Restart</button>
-        <button class="submit-button js-submit">Submit</button>
+        <button type="button" class="restart-button js-restart">Restart</button>
+        <button type="submit" class="submit-button js-submit">Submit</button>
     </form>`);
 }
 
-function renderFeedback(answer) {
-    let feedback = isCorrect(answer) ? renderRight() : renderWrong();
+function renderFeedbackPage(answer) {
+    let feedback = isCorrect(answer) ? rightFeedback() : wrongFeedback();
     $('.js-render').html(feedback);
+}
+
+function renderFinalPage() {
+    $('.js-render').html(`
+    <header>
+        <h2>You Finished The Quiz!</h2>
+    </header>
+    <p>So? Do you think you're still Elon's #1 fan? If this test made you feel insecure about your Elon Musk knowledge base, give the text another try!</p>
+    <button type="button" class = "restart-button js-restart">Restart</button>
+    `);
 }
 
 // Event listeners
 function handleStart() {
     $('.js-render').on('click', '.js-start', function (event) {
         renderQuestionPage();
+        renderCounter();
     });
 }
 
 function handleRestart() {
     $('.js-render').on('click', '.js-restart', function (event) {
-        STORE.correctAnswers = 0, STORE.incorrectAnswers = 0, STORE.currentQuestion = 0;
+        STORE.correctAnswers = 0, STORE.incorrectAnswers = 0, STORE.currentQuestionIndex = 0;
         renderStartPage();
-        renderCounter();
     });
 }
 
 function handleSubmit() {
     $('.js-render').on('click', '.js-submit', function (event) {
-        event.preventDefault();
+        // event.preventDefault();
 
         let answer = $('input[name=answer]:checked', 'form').val();
-        renderFeedback(answer);
-
+        renderFeedbackPage(answer);
         renderCounter();
+        STORE.currentQuestionIndex++;
     });
 }
 
 function handleNext() {
     $('.js-render').on('click', '.js-next', function (event) {
-        if(STORE.currentQuestion < 5) {
+        if(STORE.currentQuestionIndex < 5) {
             renderQuestionPage();
         }
         else {
-            renderFinal();
+            STORE.currentQuestionIndex -= 1;
+            renderFinalPage();
         }
         renderCounter();
     });
@@ -163,7 +126,10 @@ function handleNext() {
 
 
 function main() {
-    renderStartPage(); 
+    // Render first page
+    renderStartPage();
+
+    //Listeners
     handleStart();
     handleRestart();
     handleSubmit();
