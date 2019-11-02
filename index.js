e9f9c373b5b0b966c60d1fceff19c00f0b69fa47
Daniel Di Venere <danieldivenere@gmail.com>
2019-11-01 18:01:41 -0700

    a lot

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const STORE = {
     correctAnswers: 0,
-    incorrectAnswers: 0,
+    // incorrectAnswers: 0,
     currentQuestion: 0
 }
 
@@ -58,14 +58,40 @@ const QUESTIONS = [
 ]
 
 // Components For Renders
-function generateOptionHtml() {
-
+function generateOptionHtml(answerIndex) {
+    return `<p>
+      <input type="radio" name="answer" id="${answerIndex}" value="${QUESTIONS[STORE.currentQuestion].answers[answerIndex]}">
+      <label for="${answerIndex}">${QUESTIONS[STORE.currentQuestion].answers[answerIndex]}</label>
+    </p>`;
 }
 
+function generateAllOptionsHtml() {
+    let arrayOfOptions = QUESTIONS[STORE.currentQuestion].answers;
+    return arrayOfOptions.map((option, i) => generateOptionHtml(i)).join('');
+}
+
+function renderWrong() {
+    // Make array of insults and randomly insert them, same with right
+    return `<h2>Oof, that's wrong.</h2>
+    <p>The correct answer was: ${QUESTIONS[STORE.currentQuestion].correctAnswer}</p>
+    <button class = "next-button js-next">Next</button>`
+}
+
+function renderRight() {
+    STORE.correctAnswers++;
+    return `<h2>Elon approves!</h2>
+    <img src="happy-image">
+    <button class = "next-button js-next">Start</button>`
+}
+
+function isCorrect(givenAnswer) {
+    let correctAnswer = QUESTIONS[STORE.currentQuestion].correctAnswer;
+    return givenAnswer === correctAnswer;
+}
 
 // Rendering Functions
 function renderCounter() {
-    $('.js-counter').html(`Question: ${STORE.currentQuestion}/${QUESTIONS.length}`);
+    $('.js-counter').html(`Question: ${STORE.currentQuestion + 1}/${QUESTIONS.length}`);
     $('.score').html(`Score: ${STORE.correctAnswers}`);
 }
 
@@ -74,14 +100,27 @@ function renderStartPage() {
     <h2>Start The Quiz</h2>
     <p>Elon Musk has been pretty popular among the public lately, and that's great. But here's the problem: he's also gaining a lot of inauthentic bandwagons. 
     This quiz was designed to weed out Elon Musk's #1 fans from the fake fans jumping in on the hype. 
-    So, do you think you can prove your worth? Give it a try!</p>
-    <button class = "js-start">Start</button>
+    Do you think you can prove your worth? Give it a try!</p>
+    <button class = "start-button js-start">Start</button>
     `);
     renderCounter();
 }
 
 function renderQuestionPage() {
-    // currentQuestion for me
+    $('.js-render').html(`
+    <form>
+        <fieldset>
+            <legend>${QUESTIONS[STORE.currentQuestion].question}</legend>
+            ${generateAllOptionsHtml()}
+        </fieldset>
+        <button class="restart-button js-restart">Restart</button>
+        <button class="submit-button js-submit">Submit</button>
+    </form>`);
+}
+
+function renderFeedback(answer) {
+    let feedback = isCorrect(answer) ? renderRight() : renderWrong();
+    $('.js-render').html(feedback);
 }
 
 // Event listeners
@@ -93,25 +132,38 @@ function handleStart() {
 
 function handleRestart() {
     $('.js-render').on('click', '.js-restart', function (event) {
-
+        STORE.correctAnswers = 0, STORE.incorrectAnswers = 0, STORE.currentQuestion = 0;
+        renderStartPage();
+        renderCounter();
     });
 }
 
 function handleSubmit() {
     $('.js-render').on('click', '.js-submit', function (event) {
+        event.preventDefault();
 
+        let answer = $('input[name=answer]:checked', 'form').val();
+        renderFeedback(answer);
+
+        renderCounter();
     });
 }
 
 function handleNext() {
     $('.js-render').on('click', '.js-next', function (event) {
-
+        if(STORE.currentQuestion < 5) {
+            renderQuestionPage();
+        }
+        else {
+            renderFinal();
+        }
+        renderCounter();
     });
 }
 
 
 function main() {
-    renderStartPage(); //render but rest are listeners
+    renderStartPage(); 
     handleStart();
     handleRestart();
     handleSubmit();
